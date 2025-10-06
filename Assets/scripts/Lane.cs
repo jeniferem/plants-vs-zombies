2c8e48bb44aeed75c97de4e71603176ca2b33724
josh <jenifer.mora@uteca.edu.mx>
2025-10-06 17:49:34 -0600

    tarea

--- Assets/scripts/Lane.cs
+++ Assets/scripts/Lane.cs
@@ -4,7 +4,10 @@ public class Lane : MonoBehaviour
 {
     [SerializeField]
     private List<Transform> frames;
+    [SerializeField]
+    private Transform enemySpawnPoint;
     public List<Transform> Frames => frames;
+    public Transform EnemySpawnPoint => enemySpawnPoint;
         
 }
 
