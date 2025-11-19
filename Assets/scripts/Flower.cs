bb3c78f20de899bb3a4ad528dad979da05ca5fe8
josh <jenifer.mora@uteca.edu.mx>
2025-11-18 18:07:50 -0600

    otra_moneda

--- Assets/scripts/Flower.cs
+++ Assets/scripts/Flower.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.Events;
 
 public class Flower : BasePlant 
 {
@@ -12,6 +13,8 @@ public class Flower : BasePlant
     private InstantiatepoolObjects coinPool;
     [SerializeField]
     private float coinsOffsetY = 0.5f;
+    [SerializeField]
+    private UnityEvent<Transform> onSpawnCoin;
     private List<Step> stepsInRange = new List<Step>();
     private Coroutine spawnCoinCorutine;
     public override bool IsActive
@@ -45,6 +48,7 @@ public class Flower : BasePlant
         while (isActive && health.CurrentHealth > 0)
         {
             yield return new WaitForSeconds(flowerData.spawnCoinTime);
+            onSpawnCoin?.Invoke(transform);
             animator.Play(flowerData.GetAnimationName(ActionKey.Attack),0,0f);
             SoundManager.instance.Play(flowerData.GetSoundName(ActionKey.Attack));
             for (int i = 0; i < flowerData.coinAmount; i++)
